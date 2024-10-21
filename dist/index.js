5c02d56db9797ed8de4917ae464d5af7fa2d28c1
masashi <masashi@cizucu.com>
2024-10-21 13:10:11 +0900

    最初のコミットです

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ let count = 0;
 const counterElement = document.getElementById('counter');
 const incrementButton = document.getElementById('increment');
 const decrementButton = document.getElementById('decrement');
+const multipleButton = document.getElementById('multiple');
 function updateCounter() {
     counterElement.textContent = count.toString();
 }
@@ -12,6 +13,10 @@ incrementButton.addEventListener('click', () => {
 });
 decrementButton.addEventListener('click', () => {
     count--;
+    updateCounter();
+});
+multipleButton.addEventListener('click', () => {
+    count = count * 2;
     updateCounter();
 });
 updateCounter();
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ let count : number = 0;
 const counterElement = document.getElementById('counter') as HTMLElement;
 const incrementButton = document.getElementById('increment') as HTMLButtonElement;
 const decrementButton = document.getElementById('decrement') as HTMLButtonElement;
+const multipleButton = document.getElementById('multiple') as HTMLButtonElement;
 
 function updateCounter() {
     counterElement.textContent = count.toString();
@@ -16,6 +17,12 @@ incrementButton.addEventListener('click', () => {
 
 decrementButton.addEventListener('click', () => {
     count--;
+    updateCounter();
+}
+)
+
+multipleButton.addEventListener('click', () => {
+    count = count*2 ;
     updateCounter();
 }
 )
